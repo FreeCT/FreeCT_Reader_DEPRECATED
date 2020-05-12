43248c76a1076230ba337edae5d00a0c79ec5c07
John Hoffman <captnjohnny1618@gmail.com>
2020-05-12 07:46:51 -0700

    Improvements to include path structure for compatibility with ICD

--- include/fct_read.h
+++ include/fct_read.h
@@ -22,12 +22,12 @@
 #ifndef fct_read_h
 #define fct_read_h
 
-#include "include/binary_read.h"
-#include "include/definitionas_read.h"
-#include "include/force_read.h"
-#include "include/sensation64_v2007_read.h"
-#include "include/sensation64_v1794_read.h"
-#include "include/dicom_read.h"
-#include "include/imawrapper_read.h"
+#include "binary_read.h"
+#include "definitionas_read.h"
+#include "force_read.h"
+#include "sensation64_v2007_read.h"
+#include "sensation64_v1794_read.h"
+#include "dicom_read.h"
+#include "imawrapper_read.h"
 
 #endif
